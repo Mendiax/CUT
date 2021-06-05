9ac5846e2c8bb228441150a49ee086fcbd3d0458
Mendiax <254658@student.pwr.edu.pl>
2021-06-05 10:00:09 +0200

    remove some unnecessary code

--- test/test.c
+++ test/test.c
@@ -4,12 +4,6 @@
 #include "test_analyzer.h"
 
 int main(void) {
-#define PRINT_SIZEOF(struct) printf("sizeof(%s)= %zu\n", #struct, sizeof(struct))
-    PRINT_SIZEOF(pthread_t  );
-    PRINT_SIZEOF( sig_atomic_t );
-    PRINT_SIZEOF( AnalyzerThread );
-
-#undef PRINT_SIZEOF
     TEST_SUITE_INIT("RINGBUFFER TEST");
     TEST_CASE_RUN(ringbufferTest_full());
     TEST_CASE_RUN(ringbufferTest_pointer());
