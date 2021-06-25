e50cf2fdc88b5ddd36ea8b8fd306df9c83e8cf1c
Mendiax <254658@student.pwr.edu.pl>
2021-06-25 12:28:52 +0200

    little tweaks in main

--- app/main.c
+++ app/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-#include <assert.h>
 
 #include <reader.h>
 #include <analyzer.h>
@@ -13,18 +12,18 @@ static AnalyzerThread* analyzer;
 static PrinterThread* printer;
 static WatchdogThread* watchdog;
 static LoggerThread* logger;
-const char*  const threadsNames[] = {"watchdog", "reader", "analyzer", "printer"};
 
-volatile sig_atomic_t end = 0;
+static const char* const threadsNames[] = {"watchdog", "reader", "analyzer", "printer"};
 
 static void stop_all() {
-    int ret_printer = printer_thread_stop(printer);
-    int ret_analyzer = analyzer_thread_stop(analyzer);
-    int ret_reader = reader_thread_stop(reader);
-    int ret_watchdog = watchdog_thread_stop(watchdog);
-    int ret_logger = logger_thread_stop(logger);
+    printer_thread_stop(printer);
+    analyzer_thread_stop(analyzer);
+    reader_thread_stop(reader);
+    watchdog_thread_stop(watchdog);
+    logger_thread_stop(logger);
 }
-static void destroy_all(){
+
+static void destroy_all() {
     printer_thread_destroy(printer);
     analyzer_thread_destroy(analyzer);
     reader_thread_destroy(reader);
@@ -32,8 +31,7 @@ static void destroy_all(){
     logger_thread_destroy(logger);
 }
 
-static void int_handler(int dummy) {
-
+__attribute__((noreturn)) static void int_handler() {
     stop_all();
     destroy_all();
     exit(EXIT_SUCCESS);
@@ -41,12 +39,12 @@ static void int_handler(int dummy) {
 
 int main(void) {
     signal(SIGINT, int_handler);
-    logger = logger_thread_create("log.txt",100,10);
-    unsigned short t = (unsigned short)sysconf(_SC_NPROCESSORS_ONLN);
-    reader = reader_thread_create(logger,(unsigned short) t, 10);
-    analyzer = analyzer_thread_create(logger,reader->reader_data, (unsigned short) t, 10);
-    printer = printer_thread_create(logger,analyzer->analyzer_data);
-    watchdog = watchdog_thread_create(logger,2.0, 3, &reader->last_update, &analyzer->last_update, &printer->last_update);
+    logger = logger_thread_create("log.txt", 100, 10);
+    unsigned short t = (unsigned short) sysconf(_SC_NPROCESSORS_ONLN);
+    reader = reader_thread_create(logger, (unsigned short) t, 10);
+    analyzer = analyzer_thread_create(logger, reader->reader_data, (unsigned short) t, 10);
+    printer = printer_thread_create(logger, analyzer->analyzer_data);
+    watchdog = watchdog_thread_create(logger, 2.0, 3, &reader->last_update, &analyzer->last_update, &printer->last_update);
 
     logger_thread_start(logger);
     reader_thread_start(reader);
@@ -55,8 +53,8 @@ int main(void) {
     watchdog_thread_start(watchdog);
 
     int ret_watchdog = watchdog_thread_join(watchdog);
-    printf("%s thread is not responding\nexiting program\n",threadsNames[ret_watchdog]);
-    logger_thread_print(logger,"%s thread is not responding\nexiting program\n",threadsNames[ret_watchdog]);
+    printf("%s thread is not responding\nexiting program\n", threadsNames[ret_watchdog]);
+    logger_thread_print(logger, "%s thread is not responding\nexiting program\n", threadsNames[ret_watchdog]);
 
     stop_all();
     destroy_all();
